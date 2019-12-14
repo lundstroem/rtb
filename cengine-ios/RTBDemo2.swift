15144b1bafabdc90514bffea72cce2828b90a51b
Harry Lundström <h.lundstroem@gmail.com>
2019-12-14 22:03:25 +0100

    updated demo 2

--- cengine-ios/RTBDemo2.swift
+++ cengine-ios/RTBDemo2.swift
@@ -8,6 +8,7 @@
 
 class RTBDemo2: RTB {
 
+    var written = false
     override init(width: Int, height: Int) {
         super.init(width: width, height: height)
     }
@@ -29,28 +30,36 @@ class RTBDemo2: RTB {
                          inputBegan: Bool,
                          inputEnded: Bool) -> [UInt32] {
 
-        for x in 0...width-1 {
-            for y in 0...height-1 {
-                raster[x + y * width] = 0x333300ff
-                if (x / 2) + (x / 2) == x {
-                    raster[x + y * width] = 0x3355ffff
-                }
-                if (y / 2) + (y / 2) == y {
-                    raster[x + y * width] = 0x992233ff
-                }
-                if x == 0 || y == 0 {
-                    raster[x + y * width] = 0xffffffff
-                }
-                if y == height-1 {
-                    raster[x + y * width] = 0xff00ffff
-                }
-                if x == width-1 {
-                    raster[x + y * width] = 0xff00ffff
-                }
-                if x == 0 && x == 0 {
-                    raster[x + y * width] = 0xffffffff
+        if !written {
+            for x in 0...width-1 {
+                for y in 0...height-1 {
+                    raster[x + y * width] = 0x333300ff
+                    if (x / 2) + (x / 2) == x {
+                        raster[x + y * width] = 0x3355ffff
+                    }
+                    if (y / 2) + (y / 2) == y {
+                        raster[x + y * width] = 0x992233ff
+                    }
+                    if x == 0 || y == 0 {
+                        raster[x + y * width] = 0xffffffff
+                    }
+                    if y == height-1 {
+                        raster[x + y * width] = 0xff00ffff
+                    }
+                    if x == width-1 {
+                        raster[x + y * width] = 0xff00ffff
+                    }
+                    if x == 0 && x == 0 {
+                        raster[x + y * width] = 0xffffffff
+                    }
                 }
             }
+            written = true
+        }
+        let pixel = inputX + inputY * width
+
+        if inputActive && pixel < pixelCount {
+            raster[inputX + inputY * width] = 0xff0000ff
         }
         return raster
     }
